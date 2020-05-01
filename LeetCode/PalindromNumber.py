52820e2caead09266b1d3cb89a512d4b49e2ccee
Katia Kiseleva <katya1242@yahoo.com>
2020-05-01 01:37:01 -0700

    Added implementation to file

--- LeetCode/PalindromNumber.py
+++ LeetCode/PalindromNumber.py
@@ -1,2 +1,19 @@
 class Solution:
-    def isPalindrome() -> bool:
+    def isPalindrome(number):
+
+    # Determine whether an integer is a palindrome. An integer is a palindrome
+    # when it reads the same backward as forward.
+        temp_List = []
+        list_copy = []
+        for i in str(number):
+            temp_List.append(i)
+            list_copy.append(i)
+        list_copy.reverse()
+        print(temp_List)
+        print(list_copy)
+        if temp_List == list_copy:
+            return True
+        else:
+            return False
+    print(isPalindrome(121))
+
