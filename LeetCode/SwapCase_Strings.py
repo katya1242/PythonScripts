1bb7157fc17dac04a4b8f95b71aa530cac96e0e8
Katia Kiseleva <katya1242@yahoo.com>
2020-05-18 21:36:24 -0700

    HackerRank problem: swapping cases of characters for the incoming String.

---
+++ LeetCode/SwapCase_Strings.py
@@ -0,0 +1,23 @@
+# You are given a string and your task is to swap cases.
+# In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+"""For Example:
+Www.HackerRank.com → wWW.hACKERrANK.COM
+Pythonist 2 → pYTHONIST 2  """
+
+def swap_case(s):
+    new_string = ''
+    for character in s:
+        if character.isalpha() is not True:
+            new_string = new_string + character
+        elif character.islower() is True:
+            new_letter = character.upper()
+            new_string = new_string + new_letter
+        else:
+            new_letter = character.lower()
+            new_string = new_string + new_letter
+    return new_string
+
+s = "jhjhjGGH KJKJKJjhikoi!!!"
+result = swap_case(s)
+print(result)
